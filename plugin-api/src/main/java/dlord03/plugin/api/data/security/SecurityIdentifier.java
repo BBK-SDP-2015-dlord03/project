9760b1f54e4956d087b3a766248e0b21222aa8f7
david <dlord03@dcs.bbk.ac.uk>
2015-07-25 16:23:27 +0100

    Implement equals and hashcode on SecurityIdentifier.

--- plugin-api/src/main/java/dlord03/plugin/api/data/security/SecurityIdentifier.java
+++ plugin-api/src/main/java/dlord03/plugin/api/data/security/SecurityIdentifier.java
@@ -1,14 +1,17 @@
 package dlord03.plugin.api.data.security;
 
+
 public class SecurityIdentifier {
 
   private final IdentifierScheme scheme;
   private final String symbol;
-
+  private final int hashCode;
+  
   public SecurityIdentifier(IdentifierScheme scheme, String symbol) {
     super();
     this.scheme = scheme;
     this.symbol = symbol;
+    this.hashCode = 31 * scheme.hashCode() + symbol.hashCode();
   }
 
   public IdentifierScheme getScheme() {
@@ -19,6 +22,13 @@ public class SecurityIdentifier {
     return symbol;
   }
 
-  // TODO: hash and equals.
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (!(obj instanceof SecurityIdentifier)) return false;
+    final SecurityIdentifier other = (SecurityIdentifier) obj;
+    return (this.scheme.equals(other.scheme)
+        && this.symbol.equals(other.symbol));
+  }
 
 }
