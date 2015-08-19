96f5e83968cc278eb978545c0d2c249a92ca1030
David Lord <dlord03@dcs.bbk.ac.uk>
2015-08-19 18:22:24 +0100

    Remove web server file log from logback configuration.

--- plugin-api/src/main/java/dlord03/plugin/api/PluginException.java
+++ plugin-api/src/main/java/dlord03/plugin/api/PluginException.java
@@ -1,11 +1,7 @@
 package dlord03.plugin.api;
 
-@SuppressWarnings("serial")
 public class PluginException extends Exception {
 
-  /**
-   * 
-   */
   private static final long serialVersionUID = 362587753915516000L;
 
 }
--- plugin-api/src/main/java/dlord03/plugin/api/RecordNotFoundException.java
+++ plugin-api/src/main/java/dlord03/plugin/api/RecordNotFoundException.java
@@ -1,11 +1,7 @@
 package dlord03.plugin.api;
 
-@SuppressWarnings("serial")
 public class RecordNotFoundException extends PluginException {
 
-  /**
-   * 
-   */
   private static final long serialVersionUID = 5151830394541214936L;
 
 }
