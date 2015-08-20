e87db9591c72c272f03cad09d06bb9a39189694b
David Lord <dlord03@dcs.bbk.ac.uk>
2015-08-20 10:43:30 +0100

    JavaDoc on security's IdentifierScheme enum.

--- plugin-api/src/main/java/dlord03/plugin/api/data/security/IdentifierScheme.java
+++ plugin-api/src/main/java/dlord03/plugin/api/data/security/IdentifierScheme.java
@@ -1,7 +1,36 @@
 package dlord03.plugin.api.data.security;
 
+/**
+ * 
+ * Security identifier types are the various methods by which a security product
+ * or issue is identified. They are each managed and distributed by different
+ * organisations.
+ * 
+ * @author David Lord
+ * @formatter:off
+ *
+ */
 public enum IdentifierScheme {
 
-  ISIN, RIC, CUSIP, SEDOL, BLOOMBERG
+  /**
+   * International Securities Identification Number.
+   */
+  ISIN, 
+  /**
+   * Reuters Instrument Code.
+   */
+  RIC, 
+  /**
+   * Committee on Uniform Security Identification Procedures.
+   */
+  CUSIP, 
+  /**
+   *  Stock Exchange Daily Official List.
+   */
+  SEDOL, 
+  /**
+   * Bloomberg Global Identifier
+   */
+  BBGID
 
 }
