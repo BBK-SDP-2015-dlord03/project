d9e184afc2b6fc341e82048b9f9b659138df59d2
David Lord <dlord03@dcs.bbk.ac.uk>
2015-09-04 16:29:47 +0100

    Add some Javadoc.

--- plugin-api/src/main/java/uk/ac/bbk/dlord03/plugin/api/data/OptionType.java
+++ plugin-api/src/main/java/uk/ac/bbk/dlord03/plugin/api/data/OptionType.java
@@ -1,5 +1,19 @@
 package uk.ac.bbk.dlord03.plugin.api.data;
 
+/**
+ * 
+ * The type of an option contract.
+ * 
+ * @author David Lord
+ * @formatter:off
+ */
 public enum OptionType {
-  PUT, CALL
+    /**
+     * The option to sell the underlying asset.
+     */
+    PUT, 
+    /**
+     * The option to buy the underlying asset.
+     */
+    CALL
 }
